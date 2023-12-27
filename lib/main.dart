a06ab55f76971394d07919a01ccb2221b11e1be9
muhammadroyhan <rayhanahsan14@gmail.com>
2023-12-27 12:07:58 +0700

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:get/get.dart';
-import 'app/routes/app_pages.dart';
+import 'package:siresma/res/routes/routes.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
   SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
-  runApp(
-    GetMaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: "SUITMEDIA",
-      initialRoute: AppPages.INITIAL,
-      getPages: AppPages.routes,
-    ),
-  );
+  runApp( GetMaterialApp(
+    debugShowCheckedModeBanner: false,
+    title: "Siresma",
+    getPages: AppRoutes.appRoutes(),
+  ));
 }
+
